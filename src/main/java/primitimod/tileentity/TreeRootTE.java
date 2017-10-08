a6678f2be5a73aa709b88d90f67b7b4ae51699fc
zelazo <32609102+zelazo@users.noreply.github.com>
2017-10-08 17:13:57 +0200

    TreeRootTE - branch growing

--- src/main/java/primitimod/tileentity/TreeRootTE.java
+++ src/main/java/primitimod/tileentity/TreeRootTE.java
@@ -1,6 +1,9 @@
 package primitimod.tileentity;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 import net.minecraft.block.BlockLog;
 import net.minecraft.block.state.IBlockState;
@@ -19,8 +22,17 @@ public class TreeRootTE extends TileEntity implements ITickable {
 	private int tickCounter;
 	private final int updatePace = 20;
 
-	private static int[] trunkSectionMaxLength = new int[] { 5, 4, 6 };
 	private int[] trunkSectionCount = new int[] { 0, 0, 0 };
+
+	private static int[] trunkSectionMaxLength = new int[] { 5, 4, 6 };
+	private static int trunkMaxHeight = IntStream.of(trunkSectionMaxLength).sum();
+	
+	private static final int branchMinHeight = 5;
+	private static final int branchMaxLength = 4;
+	private static final int branchLengthDiff = 1;
+	private static final boolean canGrowMultiBranch = false;
+	
+	private Map<BlockPos, int[]> branchLength = new HashMap<>();
 	
 	@Override
 	public void update() {
@@ -35,9 +47,88 @@ public class TreeRootTE extends TileEntity implements ITickable {
 		}
 	}
 	
+	public boolean growPart(BlockPos target, GrowAction action) {
+		
+		if(action == GrowAction.TRUNK_HEIGHT) {
+			if(world.isAirBlock(target)) {
+				int size = 2;
+				
+				if(trunkSectionCount[size] < trunkSectionMaxLength[size]) {
+					
+					IBlockState newState = PrimitiModBlocks.blockComplexLog.getDefaultState()
+							.withProperty(BlockComplexLog.LOG_AXIS, BlockLog.EnumAxis.fromFacingAxis(EnumFacing.UP.getAxis()))
+							.withProperty(BlockComplexLog.SIZE, size);
+					
+					world.setBlockState(target, newState, 2);
+					changeTrunkSectionCount(size, 1);
+					
+					return true;
+				}
+				
+			}
+		}
+		else if(action == GrowAction.TRUNK_WIDTH) {
+			IBlockState targetState = world.getBlockState(target);
+			
+			if(targetState.getBlock() == PrimitiModBlocks.blockComplexLog) {
+				
+				int size = targetState.getValue(BlockComplexLog.SIZE);
+				
+				if(size != 0 && isTrunkSectionMax(size) && !isTrunkSectionMax(size-1)) {
+					
+					IBlockState newState = PrimitiModBlocks.blockComplexLog.getDefaultState()
+							.withProperty(BlockComplexLog.SIZE, size - 1 );
+					
+					world.setBlockState(target, newState, 2);
+					changeTrunkSectionCount(size-1	,  1);
+					changeTrunkSectionCount(size	, -1);
+					
+					return true;
+				}
+			}
+		}
+		else if(action == GrowAction.BRANCH) {
+			IBlockState targetState = world.getBlockState(target);
+			
+			if(targetState.getBlock() == PrimitiModBlocks.blockComplexLog) {
+				
+				int size = targetState.getValue(BlockComplexLog.SIZE);
+			
+				if(canGrowBranch(target)) {
+					EnumFacing newDir = EnumFacing.HORIZONTALS[random.nextInt(4)];
+					
+					if(!canGrowMultiBranch) {
+						
+						for(EnumFacing e : EnumFacing.HORIZONTALS) {
+							BlockPos offsetTarget = target.offset(e);
+							IBlockState offsetState = world.getBlockState(offsetTarget);
+							
+							if(offsetState.getBlock() == PrimitiModBlocks.blockComplexLog) {
+
+								BlockLog.EnumAxis axis = offsetState.getValue(BlockLog.LOG_AXIS);
+								
+								if(axis == BlockLog.EnumAxis.fromFacingAxis(e.getAxis()) ) {
+									newDir = e;
+									break;
+								}
+							}
+							
+						}
+					}
+					
+					growBranch(target, target, newDir, size);
+					return true;
+				}
+			}
+		}
+		
+		
+		return false;
+	}
+	
+	
 	public void grow(BlockPos pos) {
 		
-//		System.out.println("tick!");
 		System.out.println("tsc: "+trunkSectionCount[0]+" "+trunkSectionCount[1]+" "+trunkSectionCount[2]);
 		
 		BlockPos target = pos.offset(EnumFacing.UP);
@@ -49,42 +140,109 @@ public class TreeRootTE extends TileEntity implements ITickable {
 			if(trunkSectionCount[size] < trunkSectionMaxLength[size]) {
 				
 				IBlockState newState = PrimitiModBlocks.blockComplexLog.getDefaultState()
-						.withProperty(BlockComplexLog.LOG_AXIS, BlockLog.EnumAxis.Y)
+						.withProperty(BlockComplexLog.LOG_AXIS, BlockLog.EnumAxis.fromFacingAxis(EnumFacing.UP.getAxis()))
 						.withProperty(BlockComplexLog.SIZE, size);
 				
 				world.setBlockState(target, newState, 2);
 				changeTrunkSectionCount(size, 1);
 			}
-
-			
 			
 		}
 		else if(targetState.getBlock() == PrimitiModBlocks.blockComplexLog) {
-			int size = targetState.getValue(BlockComplexLog.SIZE);
-//
-//			if(size == 0 || random.nextBoolean()) {
-//				grow(target);
-//			}
-//			else {
-				
-				if(size != 0 && isTrunkMax(size) && !isTrunkMax(size-1)) {
-				
-					IBlockState newState = PrimitiModBlocks.blockComplexLog.getDefaultState()
-							.withProperty(BlockComplexLog.SIZE, size - 1 );
-					
-					world.setBlockState(target, newState, 2);
-					changeTrunkSectionCount(size-1	,  1);
-					changeTrunkSectionCount(size	, -1);
-				}
-				else {
-					grow(target);
-				}
 			
+			int size = targetState.getValue(BlockComplexLog.SIZE);
+				
+			if(canGrowBranch(target)) {
+				EnumFacing newDir = EnumFacing.HORIZONTALS[random.nextInt(4)];
+				
+				if(!canGrowMultiBranch) {
+					
+					for(EnumFacing e : EnumFacing.HORIZONTALS) {
+						BlockPos offsetTarget = target.offset(e);
+						IBlockState offsetState = world.getBlockState(offsetTarget);
+						
+						if(offsetState.getBlock() == PrimitiModBlocks.blockComplexLog) {
+
+							BlockLog.EnumAxis axis = offsetState.getValue(BlockLog.LOG_AXIS);
+							
+							if(axis == BlockLog.EnumAxis.fromFacingAxis(e.getAxis()) ) {
+								newDir = e;
+								break;
+							}
+						}
+						
+					}
+				}
+				
+				
+				growBranch(target, target, newDir, size);
+						
+			}
+			else if(size != 0 && isTrunkSectionMax(size) && !isTrunkSectionMax(size-1)) {
+			
+				IBlockState newState = PrimitiModBlocks.blockComplexLog.getDefaultState()
+						.withProperty(BlockComplexLog.SIZE, size - 1 );
+				
+				world.setBlockState(target, newState, 2);
+				changeTrunkSectionCount(size-1	,  1);
+				changeTrunkSectionCount(size	, -1);
+			}
+			else {
+				grow(target);
+			}
 			
 		}
 	}
 	
-	public boolean isTrunkMax(int size) {
+
+	public void growBranch(final BlockPos trunkPos, BlockPos pos, EnumFacing facing, int size) {
+		BlockPos target = pos.offset(facing);
+		IBlockState targetState = world.getBlockState(target);
+		
+		int[] branchLengths = branchLength.get(trunkPos);
+		
+		
+		int currentBranchLength = 0;
+		
+		if(branchLengths == null) {
+			branchLengths = new int[] {0, 0, 0, 0};
+			branchLength.put(trunkPos, branchLengths);
+		}
+		else {
+			System.out.println("bl: "+branchLengths.toString());
+			currentBranchLength = branchLengths[facing.getHorizontalIndex()];
+		}
+		
+		System.out.println("cbl: "+currentBranchLength + " -> " + branchLengths[facing.getHorizontalIndex()]);
+		
+		
+		if(world.isAirBlock(target)) {
+			
+			if(currentBranchLength < branchMaxLength) {
+				
+				IBlockState newState = PrimitiModBlocks.blockComplexLog.getDefaultState()
+						.withProperty(BlockComplexLog.LOG_AXIS, BlockLog.EnumAxis.fromFacingAxis(facing.getAxis()))
+						.withProperty(BlockComplexLog.SIZE, size);
+				
+				world.setBlockState(target, newState, 2);
+				
+				branchLengths[facing.getHorizontalIndex()]++;
+				branchLength.put(trunkPos, branchLengths);
+				
+			}
+			
+		}
+		else if(targetState.getBlock() == PrimitiModBlocks.blockComplexLog) {
+			growBranch(trunkPos, target, facing, size);
+		}
+	}
+	
+
+	public boolean canGrowBranch(BlockPos target) {
+		return target.getY() - this.pos.getY() > branchMinHeight;
+	}
+	
+	public boolean isTrunkSectionMax(int size) {
 		if(size >= 0 && size < trunkSectionCount.length) {
 			return trunkSectionCount[size] == trunkSectionMaxLength[size];
 		}
@@ -111,5 +269,11 @@ public class TreeRootTE extends TileEntity implements ITickable {
     public void readFromNBT(NBTTagCompound compound) {
         super.readFromNBT(compound);
         this.tickCounter = compound.getInteger("tickCounter");
+	}
+	
+	private enum GrowAction {
+		TRUNK_HEIGHT,
+		TRUNK_WIDTH,
+		BRANCH
 	}
 }
